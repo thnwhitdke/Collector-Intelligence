de7aab620cf2d9af7e3e10662196b6180f94ca44
Joe Hupp <joehupp@MacBook-Pro-5.local>
2026-06-23 18:03:48 -0400

    Force dynamic rendering for main collection page

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import Link from "next/link";
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
